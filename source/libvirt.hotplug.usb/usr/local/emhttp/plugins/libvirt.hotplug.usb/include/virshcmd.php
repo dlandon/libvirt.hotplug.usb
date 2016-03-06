028aeafcd4bc651268f32855bd404fc0b8c37ea9
bshakil <b.shakil@gmail.com>
2016-03-06 19:47:57 +0300

    Usb Xml file test

--- source/libvirt.hotplug.usb/usr/local/emhttp/plugins/libvirt.hotplug.usb/include/virshcmd.php
+++ source/libvirt.hotplug.usb/usr/local/emhttp/plugins/libvirt.hotplug.usb/include/virshcmd.php
@@ -7,27 +7,27 @@
 
 switch ($_POST['action']) {
 
-
 		case 'detach':
-
 		$vmname = $_POST['VMNAME'];
 		$usbid = $_POST['USBID'];
+		$usbstr = '';
+			if (!empty($usbid)) {
+			$usbx = explode(':', $usbid);
+			$usbstr .= "<hostdev mode='subsystem' type='usb' managed='yes'>
+						<source>
+						<vendor id='0x".$usbx[0]."'/>
+						<product id='0x".$usbx[1]."'/>
+						</source>
+						</hostdev>";
+						}
+		shell_exec("/usr/bin/echo '$usbstr' > /tmp/libvirthotplugusb.xml");
 		echo shell_exec("/sbin/ifconfig | head -n+1");
 		break;
 		
 		case 'attach':
 		$vmname = $_POST['VMNAME'];
 		$usbid = $_POST['USBID'];
-		echo shell_exec("/usr/bin/smbclient -g -L '$ip' $login 2>&1|awk -F'|' '/Disk/{print $2}'|sort");
+		echo shell_exec("/sbin/ifconfig | head -n+1");
 		break;
-		
-		
-		
-		
-		
 		}
-		
-		
-		
-
 ?>
