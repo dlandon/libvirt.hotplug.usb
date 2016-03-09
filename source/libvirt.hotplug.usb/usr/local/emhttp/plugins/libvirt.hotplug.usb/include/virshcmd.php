43403bfe0e60263726b0942e8f9aba00b038bcb7
bshakil <b.shakil@gmail.com>
2016-03-09 23:54:03 +0300

    Revert "Revert "Revert "Bug Fix"""
    This reverts commit 544caabebd01fc0b3fc9b99d65a4ee6f40dea0e7.

--- source/libvirt.hotplug.usb/usr/local/emhttp/plugins/libvirt.hotplug.usb/include/virshcmd.php
+++ source/libvirt.hotplug.usb/usr/local/emhttp/plugins/libvirt.hotplug.usb/include/virshcmd.php
@@ -22,7 +22,7 @@ $usbstr .= "<hostdev mode='subsystem' type='usb'>
 </hostdev>";
 }
 file_put_contents('/tmp/libvirthotplugusb.xml',$usbstr);
-echo shell_exec('/usr/sbin/virsh detach-device \"$vmname\" /tmp/libvirthotplugusb.xml 2>&1');
+echo shell_exec("/usr/sbin/virsh detach-device '$vmname' /tmp/libvirthotplugusb.xml 2>&1");
 break;
 		
 case 'attach':
@@ -40,7 +40,7 @@ $usbstr .= "<hostdev mode='subsystem' type='usb'>
 </hostdev>";
 }
 file_put_contents('/tmp/libvirthotplugusb.xml',$usbstr);
-echo shell_exec("/usr/sbin/virsh attach-device \"$vmname\" /tmp/libvirthotplugusb.xml 2>&1");
+echo shell_exec("/usr/sbin/virsh attach-device '$vmname' /tmp/libvirthotplugusb.xml 2>&1");
 break;
 		}
 ?>
