2382d37bcc32f454ec924130f6b2cdc4a7cc1a63
dlandon <dlandon@cinci.rr.com>
2018-03-11 15:01:05 -0400

    Webgui modifications, add help, and some code rewrite

--- source/include/virshcmd.php
+++ source/include/virshcmd.php
@@ -5,41 +5,30 @@
 ?>
 
 <?
+$vmname = $_POST['VMNAME'];
+$usbid = $_POST['USBID'];
+$usbstr = '';
+if (!empty($usbid)) 
+{
+	$usbx = explode(':', $usbid);
+	$usbstr .= "<hostdev mode='subsystem' type='usb'>
+<source>
+<vendor id='0x".$usbx[0]."'/>
+<product id='0x".$usbx[1]."'/>
+</source>
+</hostdev>";
+}
+file_put_contents('/tmp/libvirthotplugusb.xml',$usbstr);
+
 switch ($_POST['action']) {
 	case 'detach':
-		$vmname = $_POST['VMNAME'];
-		$usbid = $_POST['USBID'];
-		$usbstr = '';
-		if (!empty($usbid)) 
-		{
-			$usbx = explode(':', $usbid);
-			$usbstr .= "<hostdev mode='subsystem' type='usb'>
-			<source>
-			<vendor id='0x".$usbx[0]."'/>
-			<product id='0x".$usbx[1]."'/>
-			</source>
-			</hostdev>";
-		}
-		file_put_contents('/tmp/libvirthotplugusb.xml',$usbstr);
-		echo shell_exec("/usr/sbin/virsh detach-device '$vmname' /tmp/libvirthotplugusb.xml 2>&1");
+		$rc = shell_exec("/usr/sbin/virsh detach-device '$vmname' /tmp/libvirthotplugusb.xml 2>&1");
 		break;
 		
 	case 'attach':
-		$vmname = $_POST['VMNAME'];
-		$usbid = $_POST['USBID'];
-		$usbstr = '';
-		if (!empty($usbid)) 
-		{
-			$usbx = explode(':', $usbid);
-			$usbstr .= "<hostdev mode='subsystem' type='usb'>
-			<source>
-			<vendor id='0x".$usbx[0]."'/>
-			<product id='0x".$usbx[1]."'/>
-			</source>
-			</hostdev>";
-		}
-		file_put_contents('/tmp/libvirthotplugusb.xml',$usbstr);
-		echo shell_exec("/usr/sbin/virsh attach-device '$vmname' /tmp/libvirthotplugusb.xml 2>&1");
+		$rc = shell_exec("/usr/sbin/virsh attach-device '$vmname' /tmp/libvirthotplugusb.xml 2>&1");
 		break;
 }
+
+echo $rc;
 ?>
